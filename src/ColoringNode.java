6d982aaada5e2b0ad3bb5be5b9b014c555744402
cassie <CassandraG@gmx.fr>
2022-12-07 12:04:44 +0100

    Q1

--- src/ColoringNode.java
+++ src/ColoringNode.java
@@ -9,4 +9,23 @@ public class ColoringNode extends Node {
     public void onStart() {
         setColor(Color.getColorAt(getID())); // couleur = ID
     }
+
+    private static int posDiff(int x, int y) {
+        int p = 0;
+        while(x != 0 && y != 0) {
+            if (x % 2 != y % 2) {
+                return 2 * p + x % 2;
+            } else {
+                p++;
+                x /= 2;
+                y /= 2;
+            }
+        }
+        return -1;
+    }
+
+    private static int log2ceil(int k) {
+        double log_k = Math.log(k) / Math.log(2);
+        return (int) log_k;
+    }
 }
