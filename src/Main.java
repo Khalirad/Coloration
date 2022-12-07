92fa16a95e528fb4485748d4fbee397001529427
Amandine <asolignac@bordeaux-inp.fr>
2022-12-07 11:31:35 +0100

    Ajoût du main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import io.jbotsim.core.Topology;
+import io.jbotsim.core.Node;
+import io.jbotsim.gen.basic.TopologyGenerators;
+import io.jbotsim.ui.JTopology;
+import io.jbotsim.ui.JViewer;
+
 public class Main {
+    public final static int n = 11;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Topology tp = new Topology();
+        tp.setDefaultNodeModel(ColoringNode.class); // algo des noeuds = ColoringNode
+        TopologyGenerators.generateRing(tp, n); // topologie = cycle à n noeuds
+        tp.disableWireless(); // pour avoir un vrai anneau pour tout n
+        tp.shuffleNodeIds(); // permutation des IDs dans [0,n[
+
+        for (int i = 0; i<n; i++){ // pour chaque noeuds
+            ColoringNode u = (ColoringNode) tp.getNodes().get(i); // u = noeuds i
+            u.setLocation(u.getX()+250, u.getY()+100); // décaler sa position
+            u.parent = tp.getNodes().get((i+1) % n); // son parent
+        }
+
+        JTopology jtp = new JTopology(tp);
+        jtp.addLinkPainter(new JParentLinkPainter()); // ajoute l'orientation
+        new JViewer(jtp); // dessine la topologie
+        tp.start(); // démarre l'aglorithme
+        tp.pause(); // mode pas-à-pas
     }
 }
