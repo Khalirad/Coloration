3ee981b27b323b5ee9786f5c48574dd40540b47d
cassie <CassandraG@gmx.fr>
2022-12-07 12:23:47 +0100

    Q2 jcrois

--- src/ColoringNode.java
+++ src/ColoringNode.java
@@ -4,10 +4,22 @@ import io.jbotsim.core.Node;
 
 public class ColoringNode extends Node {
     public Node parent; // défini dans Main()
+    private int x;
+    private int l;
+
 
     @Override
     public void onStart() {
+        x = getID();
+        l = log2ceil(Main.n);
+
         setColor(Color.getColorAt(getID())); // couleur = ID
+
+        for (Node v : getNeighbors()) {
+            if (!v.equals(parent)) {
+                send(v, new Message(x));
+            }
+        }
     }
 
     private static int posDiff(int x, int y) {
@@ -27,5 +39,24 @@ public class ColoringNode extends Node {
     private static int log2ceil(int k) {
         double log_k = Math.log(k) / Math.log(2);
         return (int) log_k;
+    }
+
+    @Override
+    public void onClock() {
+        Message m = getMailbox().get(0);
+        int y = 0; // getContents je sais pas quoi j'ai pas compris
+        x = posDiff(x, y);
+        //int lp = l;
+        l = 1 + log2ceil(l);
+        for (Node v : getNeighbors()) {
+            if (!v.equals(parent)) {
+                send(v, new Message(x));
+                // à remplir
+            }
+        }
+        for (Message me : getMailbox()) {
+            //à remplir
+            ;
+        }
     }
 }
